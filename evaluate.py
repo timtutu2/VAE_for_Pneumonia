9c96d85a178365a2ced59ecc0298c7290e642053
timtutu2 <timtutu2@users.noreply.github.com>
2026-02-13 16:20:05 -0800

    eval 0.0001

--- evaluate.py
+++ evaluate.py
@@ -164,11 +164,11 @@ def compute_fid(real_dir, fake_dir, batch_size=50, device=None):
 
 def main():
     parser = argparse.ArgumentParser(description='Evaluate VAE with IS and FID')
-    parser.add_argument('--model_path', type=str, default='/mnt/tim/VAE_for_Pneumonia/outputs/mse_loss_tanh_0.005/checkpoint_epoch_80.pth',
+    parser.add_argument('--model_path', type=str, default='/mnt/tim/VAE_for_Pneumonia/outputs/mse_loss_tanh_0.0001/checkpoint_epoch_80.pth',
                         help='Path to trained model checkpoint')
     parser.add_argument('--data_dir', type=str, default='/mnt/tim/VAE_for_Pneumonia/chest_xray',
                         help='Path to chest_xray dataset directory')
-    parser.add_argument('--output_dir', type=str, default='/mnt/tim/VAE_for_Pneumonia/evaluation/mse_loss_tanh_0.005',
+    parser.add_argument('--output_dir', type=str, default='/mnt/tim/VAE_for_Pneumonia/evaluation/mse_loss_tanh_0.0001',
                         help='Directory to save evaluation results')
     parser.add_argument('--num_generated', type=int, default=1000,
                         help='Number of images to generate')
